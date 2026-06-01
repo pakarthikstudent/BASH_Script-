ec42ac7861b646a4069bda22c3012f1fbfb2fb3b
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2026-06-01 19:26:04 +0530

    Create p5.sh

---
+++ p5.sh
@@ -0,0 +1,14 @@
+read -p "Enter a disk partition1:" p1
+read -p "Enter $p1 size:" s1
+
+read -p "Enter a disk partition1:" p2
+read -p "Enter $p2 size:" s2
+total=`expr $s1 + $s2`
+
+echo "
+Partition: $p1    Size:$s1
+-------------------------------
+Partition: $p2    Size:$s2
+-------------------------------
+Total Size: $total
+-------------------------------"
