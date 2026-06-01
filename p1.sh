937703bc6ea9ec421924a74671d459c5461eb72a
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2026-06-01 19:24:38 +0530

    Create p1.sh

---
+++ p1.sh
@@ -0,0 +1,10 @@
+app_name="WebApp"
+app_port=8080
+config_fname="/etc/web/app.cfg"
+
+echo "App name is:$app_name
+---------------------------------
+$app_name running port number is:$app_port
+--------------------------------------------
+$app_name config file is:$config_fname
+----------------------------------------"
