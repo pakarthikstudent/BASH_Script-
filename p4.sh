764573b42c0cd10a8a901dcd7665b526b5236a88
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2026-06-01 19:25:45 +0530

    Create p4.sh

---
+++ p4.sh
@@ -0,0 +1,10 @@
+read -p "Enter an app name:" app_name
+read -p "Enter $app_name port number:" app_port
+read -p "Enter $app_name config filename:" config_fname
+
+echo "App name is:$app_name
+---------------------------------
+$app_name running port number is:$app_port
+--------------------------------------------
+$app_name config file is:$config_fname
+----------------------------------------"
