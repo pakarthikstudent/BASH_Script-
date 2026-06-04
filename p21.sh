e1a4226efafecaa93f07900434930b4152d5d700
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2026-06-04 19:20:55 +0530

    Create p21.sh

---
+++ p21.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 1 ]
+then
+    echo "Usage: Commandline args allow single input File"
+    echo "$0 <filename>"
+    exit
+fi
+
+if [ -f $1 ]
+then
+    echo "Yes file $1 is reg.file"
+    ls -l $1
+else
+    echo "Sorry file $1 is not reg.file"
+fi
